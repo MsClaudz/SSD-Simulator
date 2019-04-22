a773dfc05693b34a352d2afe4aad0910617152da
sgorm <sarah_gorman@outlook.com>
2019-04-22 13:46:07 -0400

    added sample FIU trace file

--- testing.py
+++ testing.py
@@ -4,7 +4,7 @@ import DictBuilder
 # trace_file = 'traces\blkparseout_ext4.txt'
 # trace_file = 'traces\blkparseout_btrfs.txt'
 # trace_file = 'traces\blkparseout_f2fs.txt'
-trace_file = 'traces\cheetah.cs.fiu.edu-110108-113008.1.blkparse'
+trace_file = 'traces\cheetah.cs.fiu.edu-110108-113008.1_sample.blkparse'
 sectors_per_block = 8
 
 test_dict = DictBuilder.build_dict(trace_file, sectors_per_block)
