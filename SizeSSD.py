62615a668af34e23aee5e54846d6884fd5407a49
sgorm <sarah_gorman@outlook.com>
2019-04-26 14:44:07 -0400

    changed "blks" to "blocks"

--- SizeSSD.py
+++ SizeSSD.py
@@ -10,15 +10,14 @@ def count_logical_blocks(freq_dict, pct_overprov):
 
     e.g. 
     >>>count_logical_blocks(freq_dict, 28)
-    300 000
+    300000
     '''
-    # remember to round up
     num_blocks = len(freq_dict)
     num_blocks = num_blocks * ((pct_overprov + 100)/100)
     num_blocks = round(num_blocks)
     return num_blocks
 
-def count_erase_blocks(num_logical_blks, erase_blk_size, logical_blk_size):
+def count_erase_blocks(num_logical_blocks, erase_block_size, sector_size, sectors_per_logical_block):
     '''(int, float, float) -> int
 
     Calculates the number of erase blocks required to accommodate the data stored in a number of logical blocks. Rounds up. 
@@ -28,15 +27,15 @@ def count_erase_blocks(num_logical_blks, erase_blk_size, logical_blk_size):
     >>>count_erase_blocks(300 000, 4096.0, 4.096)
     300
 
-    >>>count_erase_blocks((count_logical_blocks(freq_dict, 28)), 4096.0, (sector_size * 0.512))
+    >>>count_erase_blocks((count_logical_blocks(freq_dict, 28)), 4096.0, (sectors_per_block * 0.512))
     300
     '''
     # test this
-    return (num_logical_blks * logical_blk_size) / erase_blk_size 
+    return (num_logical_blocks * logical_block_size) / erase_block_size 
     # round up
 
 
-def blocks_per_partition(num_erase_blks, num_partitions):
+def blocks_per_partition(num_erase_blocks, num_partitions):
     '''(int, int) -> int
 
     Calculates the number of erase blocks to allocate to each SSD partition. Rounds up
@@ -46,7 +45,7 @@ def blocks_per_partition(num_erase_blks, num_partitions):
     100
     '''
     # test this
-    return num_erase_blks/num_partitions
+    return num_erase_blocks/num_partitions
     # round up
 
 # Also compute max number of logical blocks per erase block and number of overprovisioned erase blocks per partition (see notes in MakeSSD)
