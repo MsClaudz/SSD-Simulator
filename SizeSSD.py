1b5e02e7759bdc4226eb6e07667133bc96e21b2d
sgorm <sarah_gorman@outlook.com>
2019-04-28 22:56:26 -0400

    Improved method for rounding numbers

--- SizeSSD.py
+++ SizeSSD.py
@@ -23,33 +23,31 @@ def count_logical_blocks(trace_dict):
 
    
 def calculate_num_erase_blocks(num_logical_blocks, logical_block_size_in_KB, physical_page_size_in_KB, pages_per_erase_block, percent_of_overprovisioning):
-    '''(int, int, int, int) -> int
+    '''(int, int, int, int) -> (float, float)
 
     Takes a number of logical blocks, the number of flash pages per logical block, the number of flash pages per erase block, 
     and a desired percentage of overprovisioning, and returns the number of main and overprovisioned erase blocks required 
-    to accommodate the data stored in the number of logical blocks. Returns a tuple with both values rounded to whole numbers.
+    to accommodate the data stored in the number of logical blocks. Returns a tuple with both values.
     
     >>>calculate_num_erase_blocks(32, 4.096, 0.512, 128, 28)
-    (2, 1)
+    (2.0, 0.56)
 
     >>>calculate_num_erase_blocks(300000, 4.096, 4.096, 128, 28)
-    (2344, 656)
+    (2343.75, 656.2500000000001)
     '''
     physical_pages_per_logical_block = logical_block_size_in_KB/physical_page_size_in_KB
-    num_main_erase_blocks = round((num_logical_blocks * physical_pages_per_logical_block)/pages_per_erase_block)
-    if num_main_erase_blocks == 0:
-        num_main_erase_blocks = 1
-    num_overprovisioned_erase_blocks = round(num_main_erase_blocks * (percent_of_overprovisioning/100))
+    num_main_erase_blocks = (num_logical_blocks * physical_pages_per_logical_block)/pages_per_erase_block
+    num_overprovisioned_erase_blocks = num_main_erase_blocks * (percent_of_overprovisioning/100)
     return (num_main_erase_blocks, num_overprovisioned_erase_blocks)
 
 
 def main_blocks_per_partition(num_main_erase_blocks, num_partitions):
-    '''(int, int) -> int
+    '''(float, int) -> int
 
-    Returns the number of erase blocks to allocate to each SSD partition.
+    Returns the number of erase blocks to allocate to each SSD partition. Rounds to nearest whole number.
 
     e.g.
-    >>>blocks_per_partition(300, 3)
+    >>>blocks_per_partition(300.0, 3)
     100
     '''
-    return int(num_main_erase_blocks/num_partitions)
+    return round(num_main_erase_blocks/num_partitions)
