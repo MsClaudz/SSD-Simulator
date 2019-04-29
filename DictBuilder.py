4f319dabca62f3973d6efce409c70b0318f11bd8
sgorm <sarah_gorman@outlook.com>
2019-04-29 01:06:36 -0400

    Further changes to DictBuilder for SimulateIO

--- DictBuilder.py
+++ DictBuilder.py
@@ -39,29 +39,36 @@ def get_blocks(event, logical_block_size_in_KB, logical_sector_size_in_KB):
 
 def filter_event(event):
     '''(list of str) -> bool
-    Returns True if trace event is a write event and a complete event
-    e.g.
+    
+    Returns True if trace event is a valid, complete write event.
+    
+    e.g. (note that examples are based on our trace data, not FIU trace data)
     >>>filter_event(['8,16', '2', '14722', '73.165905398', '0', 'C', 'WS', '12858376', '+', '32', '[0]'])
     True
+
     >>>filter_event(['8,16', '2', '10013', '16.774178596', '3929', 'C', 'R', '5149344', '+', '120', '[0]'])
     False
-    >>>filter_event(['8,16', '1', '14483', '615.865377616', '3997', 'D', 'W', '7240192', '+', '1536', '[kworker/u8:0]'])
-    False
     '''
+    try:
+        # Our traces
+        # If the event is not some type of write ('W', 'WS', or 'WM'), or not the completed part of the transaction ('C') return False
+        # if (event[6][0] != 'W') or (event[5] != 'C'):
+            # return False
+        # else:
+            # return True
 
-    # Our traces
-    # If the event is not some type of write ('W', 'WS', or 'WM'), or not the completed part of the transaction ('C') return False
-    # if (event[6][0] != 'W') or (event[5] != 'C'):
-        # return False
+        # FIU traces
+        # If event is not some type of write ('W', 'WS', or 'WM'), return false
+        if event[5][0] != 'W':
+            return False
+        else:
+            return True   
     
-    # FIU traces
-    # If event is not some type of write ('W', 'WS', or 'WM'), return false
-    if event[5][0] != 'W':
+    # Exclude lines from beginning and end of the file that are not trace events
+    except IndexError:
         return False
-    else:
-        return True
 
-
+        
 def add_to_dict(blocks, freq_dict):
     '''(list of int, dict of int:int) -> dict of int:int
     
@@ -97,11 +104,12 @@ def add_to_dict(blocks, freq_dict):
 def build_dict(trace_file, logical_block_size_in_KB, logical_sector_size_in_KB):
     '''(txt file, int) -> dict of int:int
 
-    Reads data from a trace file line-by-line and skips lines that are non-write events or incomplete events.
-    For complete write events, calls get_blocks then add_to_dict to populate a dictionary with logical block update frequencies.
+    Reads data from a trace file line-by-line and calls filter_event to skip lines that are non-write events or incomplete events.
+    For valid, complete write events, calls get_blocks then add_to_dict to populate a dictionary with logical block update frequencies.
+    Takes a file of trace data and logical block and sector sizes for the file system from which the traced requests were issued.
 
     e.g.
-    >>>build_dict('blkparseout.txt', 8)
+    >>>build_dict('blkparseout.txt', 4.096, 0.512)
     {6810720: 1, 6810721: 3, 6810722: 2, 6810723: 5, 6810724: 8}
     '''
     freq_dict = {}
@@ -110,15 +118,14 @@ def build_dict(trace_file, logical_block_size_in_KB, logical_sector_size_in_KB):
     # Read events from the trace file and put in a list one-by-one
     for event in trace_data:
         event = event.split()
-    # For each event, use filter_event check if it's a complete write and if so, update dict
-        try:
-            if filter_event(event) == False:
-                continue
-            else:
-                add_to_dict((get_blocks(event, logical_block_size_in_KB, logical_sector_size_in_KB)), freq_dict)
-                continue
-    # But, exclude lines from the file that are not trace events
-        except IndexError:
+
+    # For each event, use filter_event to check if it's a valid, complete write event and if so, update dict
+        if filter_event(event) == False:
             continue
-    # Return updated dict
+        else:
+            add_to_dict((get_blocks(event, logical_block_size_in_KB, logical_sector_size_in_KB)), freq_dict)
+            continue
+
+    # Close file and return updated dict
+    trace_data.close()
     return freq_dict
