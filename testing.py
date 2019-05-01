4b6bae86c119ea9226b34168b3c5cecc6f3374ec
Claudz <claudzchen@gmail.com>
2019-05-01 14:01:56 -0400

    print SSD size and start and end time stamp

--- testing.py
+++ testing.py
@@ -4,9 +4,11 @@ import Partitioning
 import SizeSSD
 import MakeSSD
 import SimulateIO
+import datetime
 
 # Choose sample file and set parameters
 trace_file = 'traces\cheetah.cs.fiu.edu-110108-113008.1_sample.blkparse'
+#trace_file = 'traces\cheetah.cs.fiu.edu-110108-113008.2.blkparse'
 # trace_file = 'traces\cheetah.cs.fiu.edu-110108-113008.1_sample_100,000_lines'
 # trace_file = 'traces\cheetah.cs.fiu.edu-110108-113008.1_sample_10000_lines'
 logical_block_size_in_KB = 4.096 # default for ext4
@@ -18,6 +20,9 @@ num_partitions = 2
 UFR_or_NP = 'UFR'
 percent_of_overprovisioning = 28
 provisioning_is_static = True
+
+starttime = datetime.datetime.now()
+print(starttime)
 
 # Create dictionary using DictBuilder
 print("\nbuilding dictionary...")
@@ -56,6 +61,12 @@ main_blocks_per_partition = SizeSSD.main_blocks_per_partition(num_main_erase_blo
 print("total number of main erase blocks required:", num_main_erase_blocks)
 print("number of main erase blocks required per partition:", main_blocks_per_partition)
 print("total number of overprovisioned erase blocks required:", num_overprovisioned_erase_blocks)
+main_size = num_main_erase_blocks * pages_per_erase_block * physical_page_size_in_KB * 0.000001
+op_size = num_overprovisioned_erase_blocks * pages_per_erase_block * physical_page_size_in_KB * 0.000001
+total_SSD_size = main_size + op_size
+print("total main erase block size in GB: ", main_size)
+print("total overprovisioned size in GB: ", op_size)
+print("total SSD size in GB: ", total_SSD_size)
 
 # make SSD structure
 print("\nmaking SSD...")
@@ -66,4 +77,6 @@ print("SSD made")
 print("\nSimulating SSD writes... this will take a while...")
 write_amplification = SimulateIO.Run_IO(trace_file, logical_block_size_in_KB, logical_sector_size_in_KB, partition_dict, SSD, pages_per_erase_block, main_blocks_per_partition, provisioning_is_static)
 print('write_amplification:', write_amplification)
+print("\n start time:", starttime)
+print(" end time:",datetime.datetime.now())
 print("\ndone")
